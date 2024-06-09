30b3e888bda1fb512be05ff8786423294da7260a
dave.tamakloe <dave.tamakloe@gmail.com>
2024-06-09 12:07:42 +0000

    config

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    if not SECRET_KEY:
-        raise ValueError("SECRET_KEY is not set")
+    # if not SECRET_KEY:
+    #     raise ValueError("SECRET_KEY is not set")
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///bed_management.db'
     if not SQLALCHEMY_DATABASE_URI:
         raise ValueError("DATABASE_URL is not set")

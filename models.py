79c12830353ca2ac26b7cc904e3db706c3ea6622
dave.tamakloe <dave.tamakloe@gmail.com>
2024-06-09 12:06:17 +0000

    handle user registration and login

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from database import db
-from flask-bcrypt import Bcrypt
+from flask_bcrypt import Bcrypt
 
 
 bcrypt = Bcrypt()
@@ -7,7 +7,7 @@ bcrypt = Bcrypt()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullabble=False)
+    password_hash = db.Column(db.String(128), nullable=False)
 
     def set_password(self, password):
         self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
@@ -16,33 +16,33 @@ class User(db.Model):
         return bcrypt.check_password_hash(self.password_hash, password)
     
 
-class Hospital(db.model):
+class Hospital(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     location = db.Column(db.String(255), nullable=False)
-    departments = db.relationship('Department', backref='hospial', lazy=True)
+    departments = db.relationship('Department', backref='hospital', lazy=True)
 
 
 class Department(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
-    hospital_id = db.column(db.Integer, db.ForeignKey('hospital.id') nullable=False)
+    hospital_id = db.Column(db.Integer, db.ForeignKey('hospital.id'), nullable=False)
     beds = db.relationship('Bed', backref='department', lazy=True)
    
     
-class Bed(db.model):
+class Bed(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    department_id = db.Column(db.Integer, db.ForeignKey('hospital.id'), nullable=False)
-    bed_number = db.Column(db.Integer, db.ForeignKey('department_id'), nullable=False)
-    status = db.Column(db.String(50),nullable=False, default='available')
+    department_id = db.Column(db.Integer, db.ForeignKey('department.id'), nullable=False)
+    bed_number = db.Column(db.Integer, nullable=False)
+    status = db.Column(db.String(50), nullable=False)
     patients = db.relationship('Patient', backref='bed', lazy=True)
     
 
-class Patient(db.model):
+class Patient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
-    age = db.Column(db.integer, nullable=False)
-    bed_id = db.Column(db.Integer, db.ForeignKey('bed_id'), nullable=True)
+    age = db.Column(db.Integer, nullable=False)
+    bed_id = db.Column(db.Integer, db.ForeignKey('bed.id'), nullable=True)
     
 
 
---
+++ resources/auth.py
@@ -0,0 +1,38 @@
+from flask import request
+from flask_restful import Resource
+from models import User
+from database import db
+from flask_jwt_extended import create_access_token
+
+# User Registration API
+class UserRegistration(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+
+        if User.query.filter_by(username=username).first():
+            return {'message': 'User already exists'}, 409
+
+            new_user = User(username=username)
+            new_user.set_password(password)
+            db.session.add(new_user)
+            db.session.commit()
+
+            return {'message': 'User created successfully'}, 201
+
+            
+# User Login API
+class UserLogin(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.check_password(password):
+            access_token = create_access_token(identity={'username':username})
+            return {'access_token': access_token}, 200
+        else:
+            return {'message': 'Invalid credentials'}, 401
